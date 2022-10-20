f96305618fb9c8a247e0fe39c2caf7532817d66f
Jimmy Juarez <jimjuareza@gmail.com>
2022-10-20 11:54:32 -0700

    winning logic pt 2

--- web.js
+++ web.js
@@ -10,36 +10,13 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  
-  // TODO add your implementation here to replace the random response
-  if(index >= 3){
-    index = 0;
+  const moves = ['F','T'];
+  index++
+  if(index >= moves.length){
+    index = 0
   }
-  index++;
+  // TODO add your implementation here to replace the random response
   res.send(moves[index]);
 });
 
 app.listen(process.env.PORT || 8080);
-// const express = require('express');
-// const app = express();
-// const bodyParser = require('body-parser');
-// let index = 0;
-// app.use(bodyParser.json());
-
-// app.get('/', function (req, res) {
-//   res.send('Let the battle begin!');
-// });
-
-// app.post('/', function (req, res) {
-//   console.log(req.body);
-//   const moves = ['F','T'];
-//   index++
-//   if(index >= moves.length){
-//     index = 0
-//   }
-//   // TODO add your implementation here to replace the random response
-//   res.send(moves[index]);
-// });
-
-// app.listen(process.env.PORT || 8080);
